d8d134ded3c88c9bdaccfe5c6cf452e1653f5f8b
mike dupont <mike.dupont@introspector.local>
2025-01-09 20:35:21 -0500

    using agent not eliza

--- rundocker.sh
+++ rundocker.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 # this is the install script 
-#  install_script = "/opt/eliza/rundocker.sh"
+#  install_script = "/opt/agent/rundocker.sh"
 # called on boot.
 pwd
 ls -latr
 . ./.env # for secrets
 set -e # stop  on any error
-export WORKSOURCE="/opt/eliza"
+export WORKSOURCE="/opt/agent"
 snap install aws-cli --classic
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 916723593639.dkr.ecr.us-east-2.amazonaws.com
-adduser --disabled-password --gecos "" eliza --home "/home/eliza"  || echo ignore
-git config --global --add safe.directory "/opt/eliza"
-git config --global --add safe.directory "/opt/eliza-memory"
+adduser --disabled-password --gecos "" agent --home "/home/agent"  || echo ignore
+git config --global --add safe.directory "/opt/agent"
 cd "/opt/agent/" || exit 1 # "we need agent"
-git log -2 --patch | head  -1000
+git log -1 
 mkdir -p "/home/agent"
 mkdir -p "/var/agent/logs"
 chown -R agent:agent "/var/agent/" "/home/agent" "/opt/agent"
 mkdir -p "/var/run/agent/secrets/"
-mkdir -p "/home/agent/.cache/huggingface/hub"
 
 ## TURN OFF LOGGING
 set +x
@@ -29,9 +27,6 @@ echo "OPENAI_API_KEY=${OPENAI_KEY}" >> "/var/run/agent/secrets/env"
 set -x
 ## TURN ON LOGGING
 
-if ! grep -q "HF_HOME" "/var/run/agent/secrets/env"; then
-    echo "HF_HOME=/home/agent/.cache/huggingface/hub" >> "/var/run/agent/secrets/env"
-fi
 if ! grep -q "^HOME" "/var/run/agent/secrets/env"; then
     echo "HOME=/home/agent" >> "/var/run/agent/secrets/env"
 fi
