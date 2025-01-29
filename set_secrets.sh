d96dcea17ab41e31e94fc3218e2b450ac0d2a324
Deadsg <144394753+Deadsg@users.noreply.github.com>
2025-01-29 09:38:26 -0500

    Update set_secrets.sh

--- set_secrets.sh
+++ set_secrets.sh
@@ -4,14 +4,14 @@ export TOKENIZER_IMAGE=h4ckermike/elizaos-eliza:feature-arm64_fastembed
 
 # sets the parameter
 #aws ssm put-parameter     --name "agent_openai_key"  --value "${OPENAI_API_KEY}" --type String
-aws ssm put-parameter --overwrite --name "tine_agent_twitter_password"  --value "${TWITTER_PASSWORD}" --type String 
-aws ssm put-parameter --overwrite --name "tine_agent_twitter_email"  --value "${TWITTER_EMAIL}" --type String
-aws ssm put-parameter --overwrite --name "tine_agent_twitter_username"  --value "${TWITTER_USERNAME}" --type String
+aws ssm put-parameter --overwrite --name "tine_agent_twitter_password"  --value "${Blood0523}" --type String 
+aws ssm put-parameter --overwrite --name "tine_agent_twitter_email"  --value "${7817101326}" --type String
+aws ssm put-parameter --overwrite --name "tine_agent_twitter_username"  --value "${@batsy235}" --type String
 #aws ssm put-parameter     --name "tine_agent_openai_key"  --value "${OPENAI_API_KEY}" --type String
 #aws ssm put-parameter     --name "tine_agent_openai_endpoint"  --value "${OPENAI_API_BASE}" --type String
 #aws ssm put-parameter     --name "tine_agent_openai_model"  --value "${LLMMODEL}" --type String
-aws ssm put-parameter     --name "tine_agent_groq_key"  --value "${GROQ_API_KEY}" --type String
+aws ssm put-parameter     --name "tine_agent_groq_key"  --value "${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}" --type String
 
-aws ssm put-parameter     --name "tine_agent_agent_image"  --value "${AGENT_IMAGE}" --type String
-aws ssm put-parameter     --name "tine_agent_tokenizer_image"  --value "${TOKENIZER_IMAGE}" --type String
+aws ssm put-parameter     --name "tine_agent_agent_image"  --value "${h4ckermike/elizaos-eliza:feature-arm64_fastembed}" --type String
+aws ssm put-parameter     --name "tine_agent_tokenizer_image"  --value "${h4ckermike/arm64-tokenizers:feature-arm64}" --type String
 
