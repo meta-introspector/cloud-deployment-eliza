0773bc4d60f0bb0191615a86bb8f0f4b6ca14958
mike dupont <mike.dupont@introspector.local>
2025-02-02 11:29:50 -0500

    untested: diagnosis steps
    adding more debug statements to help diagnose the agent variables

--- get_secrets.sh
+++ get_secrets.sh
@@ -1,24 +1,26 @@
+#!/bin/bash
 # we are using parameters prefixed by tine_ for multiple 
 ## TURN OFF LOGGING
+echo using "${AGENT_NAME}" as agent name base for keys
 set +x
 
 #this script expects AGENT_NAME to be set to something like "tine_agent"
 
 echo "" > "/var/run/agent/secrets/env" # blank the file
 
-declare -A params=(
 #  b ["OPENAI_KEY"]="${AGENT_NAME}_openai_key"
 #   ["XAI_MODEL"]="${AGENT_NAME}_openai_model"
 #   ["XAI_L_MODEL"]="${AGENT_NAME}_large_openai_model"
 #   ["XAI_M_MODEL"]="${AGENT_NAME}_medium_openai_model"
 #   ["OPENAI_API_URL"]="${AGENT_NAME}_openai_endpoint"
-    ["GROQ_API_KEY"]="${AGENT_NAME}_groq_key"
-    ["TWITTER_PASSWORD"]="${AGENT_NAME}_twitter_password"
-    ["TWITTER_EMAIL"]="${AGENT_NAME}_twitter_email"
-    ["TWITTER_USERNAME"]="${AGENT_NAME}_twitter_username"
-    ["AGENT_IMAGE"]="${AGENT_NAME}_agent_image"
-    ["TOKENIZER_IMAGE"]="${AGENT_NAME}_tokenizer_image"
-)
+
+declare -A params=(["GROQ_API_KEY"]="${AGENT_NAME}_groq_key"
+		   ["TWITTER_PASSWORD"]="${AGENT_NAME}_twitter_password"
+		   ["TWITTER_EMAIL"]="${AGENT_NAME}_twitter_email"
+		   ["TWITTER_USERNAME"]="${AGENT_NAME}_twitter_username"
+		   ["AGENT_IMAGE"]="${AGENT_NAME}_agent_image"
+		   ["TOKENIZER_IMAGE"]="${AGENT_NAME}_tokenizer_image"
+		  )
 
 #systemctl set-environment enterUser=my-username
 for key in "${!params[@]}"; do
