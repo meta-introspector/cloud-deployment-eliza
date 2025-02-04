ccb36f78b4ea5a86c08037240ea95f5dc2176d9d
mike dupont <mike.dupont@introspector.local>
2025-02-04 15:13:53 -0500

    replace flake

--- flake.nix
+++ flake.nix
@@ -1,42 +1,76 @@
-# {
-#   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#   inputs.flake-utils.url = "github:numtide/flake-utils";
-#   outputs = { self, nixpkgs, flake-utils }:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#         inherit (import ./default.nix { inherit pkgs; })     sources package shell nodeDependencies;
-#       in {
-#         nodeDependencies = nodeDependencies;
-#         inherit overlays;
-#       });
-# }
 {
+  description = "Eliza TypeScript Project Flake";
+
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    { systems, nixpkgs, ... }@inputs:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          # Shell environment setup for development
           buildInputs = [
             pkgs.nodejs
-            # You can set the major version of Node.js to a specific one instead
-            # of the default version
-            # pkgs.nodejs-22_x
-            # Comment out one of these to use an alternative package manager.
-            # pkgs.yarn
-            # pkgs.pnpm
-            # pkgs.bun
-            #pkgs.nodePackages.typescript
-            #pkgs.nodePackages.typescript-language-server
+            pkgs.nodePackages.typescript
           ];
+
+          # Optional: Set some environment variables if needed
+          shellHook = ''
+            PS1="[node-nix]: "
+
+            echo "Welcome to the Node.js TypeScript development shell!"
+          '';
+        };
+
+        # Build process: You can customize this if you have specific build steps
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "node-nix";
+          version = "1.0.0";
+
+          # Use the project directory (might be good to limit this to just source at some point)
+          src = ./.;
+
+          # Build dependencies for building the production package
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.nodePackages.typescript
+            pkgs.cacert
+          ];
+
+          # NPM install and TypeScript compile
+          buildPhase = ''
+            # Need for NPM to work
+            mkdir -p tmp-npm
+            HOME=tmp-npm
+
+            npm install
+            npm run build
+          '';
+
+          # Out the things we need to the output
+          installPhase = ''
+            mkdir -p            $out/lib/
+            cp -r out           $out/lib/
+            cp    package.json  $out/lib/
+            cp -r node_modules  $out/lib/
+          '';
+
+          # Set metadata
+          meta = with pkgs.lib; {
+            description = "A Node.js TypeScript project built with Nix";
+            license = licenses.mit;
+            platforms = platforms.all;
+          };
+
+          # Fixed output derivation. Means we can do impure things like access the internet (for NPM to work) as long as we lock down the output hash
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+          outputHash = "sha256-Zah7U1zkOQdeIduKrWN3/Yryxa4wS+MK20FQIgUvjSA=";
         };
       });
-    };
 }
