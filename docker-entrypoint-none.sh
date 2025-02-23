576571d1cfcb8f579d85ca16e6c483ee59a9af5e
mike dupont <mike.dupont@introspector.local>
2025-02-23 05:37:00 -0500

    just load all the chars in the char directory,
    If you dont want them, delete them.

--- docker-entrypoint-none.sh
+++ docker-entrypoint-none.sh
@@ -13,6 +13,7 @@ export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 corepack enable && corepack install --global pnpm@9.8.0
 
 #strace -f -o /opt/agent/strace.log -s99999 node CMD ["pnpm", "start", "--characters=characters/eliza.character.json"]
-pnpm start --characters=characters/tine-test.character.json
+#pnpm start --characters=characters/tine-test.character.json
+pnpm start --characters=$(ls -1p characters/*.json |  paste -sd,)
 #fi
 #exec "$@"
--- docker-entrypoint-strace2.sh
+++ docker-entrypoint-strace2.sh
@@ -14,6 +14,6 @@ corepack enable && corepack install --global pnpm@9.8.0
 
 
 #strace -f -o /opt/agent/strace.log -s99999 node CMD ["pnpm", "start", "--characters=characters/eliza.character.json"]
-strace -f -o /opt/agent/strace.log -s99999 pnpm start --characters=characters/tine-test.character.json
+strace -f -o /opt/agent/strace.log -s99999 pnpm start:debug --characters=$(ls -1p characters/*.json |  paste -sd,)
 #fi
 #exec "$@"
