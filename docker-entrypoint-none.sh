e9a8ac9a9ba15d003e9f59629e5aa6cf14cd2100
mike dupont <mike.dupont@introspector.local>
2025-03-15 12:07:09 -0400

    update scripts

--- docker-entrypoint-none.sh
+++ docker-entrypoint-none.sh
@@ -9,11 +9,12 @@ set -x
 #apt update
 #apt install -y strace
 
-export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
-corepack enable && corepack install --global pnpm@9.8.0
+#export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+#corepack enable && corepack install --global pnpm@9.8.0
 
 #strace -f -o /opt/agent/strace.log -s99999 node CMD ["pnpm", "start", "--characters=characters/eliza.character.json"]
 #pnpm start --characters=characters/tine-test.character.json
-pnpm start --characters=$(ls -1p characters/*.json |  paste -sd,)
+#pnpm start --characters=$(ls -1p characters/*.json |  paste -sd,)
 #fi
 #exec "$@"
+bun run start
