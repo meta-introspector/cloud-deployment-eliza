dbe1cdb42f2d1c63057aed6fb395729ba4aeaa03
mike dupont <mike.dupont@introspector.local>
2025-04-15 11:18:49 -0400

    Adding task updates

---
+++ packages/plugin-sql/drizzle/migrations/0001_task_upgrade.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "memories" DROP CONSTRAINT "fk_agent";
+--> statement-breakpoint
+ALTER TABLE "agents" ALTER COLUMN "enabled" SET DEFAULT true;--> statement-breakpoint
+ALTER TABLE "memories" ADD CONSTRAINT "fk_agent" FOREIGN KEY ("agentId") REFERENCES "public"."agents"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_room_id_rooms_id_fk" FOREIGN KEY ("room_id") REFERENCES "public"."rooms"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_world_id_worlds_id_fk" FOREIGN KEY ("world_id") REFERENCES "public"."worlds"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_agent_id_agents_id_fk" FOREIGN KEY ("agent_id") REFERENCES "public"."agents"("id") ON DELETE cascade ON UPDATE no action;
